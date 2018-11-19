10e7520bf3ebe0f84a50fc488284d6c38bee195c
Anton Bialenik <tony.baton@yandex.by>
2018-11-19 11:15:08 +0300

    full app

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ftp ftpClient = new ftp(@"ftp://192.168.1.149/", "user-ftp", "1234");
+            //ftpClient.download("/1/.", @"D:\downloaded\");
+            ftpClient.upload("/2/pass.txt", @"D:\an.txt");
+        }
+    }
+}
